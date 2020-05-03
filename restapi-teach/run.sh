b5b05ab95c9d688042a7a89c835094849bd199e0
weiguozhen <39047882+weiguozhen@users.noreply.github.com>
2020-05-03 13:03:21 +0800

    Update run.sh

--- restapi-teach/run.sh
+++ restapi-teach/run.sh
@@ -1,21 +1,3 @@
 source /etc/profile
 #!/bin/bash
-### BEGIN INIT INFO
-
-# Provides:          init_frpc.sh
-
-# Required-start:    $local_fs $remote_fs $network $syslog
-
-# Required-Stop:     $local_fs $remote_fs $network $syslog
-
-# Default-Start:     2 3 4 5
-
-# Default-Stop:      0 1 6
-
-# Short-Description: starts the init_frpc.sh daemon
-
-# Description:       starts init_frpc.sh using start-stop-daemon
-
-### END INIT INFO
-
-cd backend && nohup python3 project/cherrypy_startup.py
+cd backend && nohup python3 project/cherrypy_startup.py >& run.log &
